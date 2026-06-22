3e80fcc7e1029a818872d3d8ea5fcef895e6960c
kimm-jin <acws12@nate.com>
2026-06-22 11:51:06 +0900

    java-spring-boot ex_1_3

--- spring-theory/spring-theory/src/main/java/com/example/spring/springtheory/ch01/ex_1_3/dao/DConnectionMaker.java
+++ spring-theory/spring-theory/src/main/java/com/example/spring/springtheory/ch01/ex_1_3/dao/DConnectionMaker.java
@@ -1,13 +1,15 @@
 package com.example.spring.springtheory.ch01.ex_1_3.dao;
 
-import com.example.spring.springtheory.ch01.ex_1_2.dao.SimpleConnectionMaker_2;
-
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DConnectionMaker implements SimpleConnectionMaker_2 {
+public class DConnectionMaker implements SimpleConnectionMaker {
     @Override
     public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
-        return null;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/springtheory", "root", "1234");
+
+        return conn;
     }
 }
