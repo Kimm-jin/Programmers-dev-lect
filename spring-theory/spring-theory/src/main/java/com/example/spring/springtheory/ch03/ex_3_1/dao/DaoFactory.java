4385ba4ed2fbc6402db9bc23a771b54ca963dcc6
kimm-jin <acws12@nate.com>
2026-06-24 12:07:45 +0900

    spring theory ex_3_2

--- spring-theory/spring-theory/src/main/java/com/example/spring/springtheory/ch03/ex_3_1/dao/DaoFactory.java
+++ spring-theory/spring-theory/src/main/java/com/example/spring/springtheory/ch03/ex_3_1/dao/DaoFactory.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정 정보라는 표시
 public class DaoFactory {
 
-    @Bean // 오브젝트 생성을 담당하는 IoC용 메서드라는 표시
-    public UserDAO userDAO() {
-        return new UserDAO(connectionMaker());
-    }
+//    @Bean // 오브젝트 생성을 담당하는 IoC용 메서드라는 표시
+//    public UserDAO userDAO() {
+//        return new UserDAO(connectionMaker());
+//    }
 
     @Bean
     public SimpleConnectionMaker connectionMaker() {
