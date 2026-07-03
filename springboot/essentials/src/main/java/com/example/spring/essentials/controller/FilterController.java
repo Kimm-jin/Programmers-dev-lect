4974be5aa90abd95aeb69ec68a51540973382f80
kimm-jin <acws12@nate.com>
2026-07-03 09:42:10 +0900

    essentials

--- springboot/essentials/src/main/java/com/example/spring/essentials/controller/FilterController.java
+++ springboot/essentials/src/main/java/com/example/spring/essentials/controller/FilterController.java
@@ -1,4 +1,4 @@
-package org.example.essentials.controller;
+package com.example.spring.essentials.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
