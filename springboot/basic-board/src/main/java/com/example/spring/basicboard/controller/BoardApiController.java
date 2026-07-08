8325deac242949fb6aa98a00b1be6b6498d4d1b5
kimm-jin <acws12@nate.com>
2026-07-08 11:22:11 +0900

    basic-board

--- springboot/basic-board/src/main/java/com/example/spring/basicboard/controller/BoardApiController.java
+++ springboot/basic-board/src/main/java/com/example/spring/basicboard/controller/BoardApiController.java
@@ -83,6 +83,16 @@ public class BoardApiController {
                 .replaceAll("\\+", "%20");
         //   - URLEncoder 는 공백을 '+' 로 바꾸는데, 파일명에선 '+' 가 그대로 보이면 곤란하므로 %20 으로 치환한다
 
+
+        // * contentType(MediaType.APPLICATION_OCTET_STREAM)
+        // 무슨 파일인지 특정하지 않은 순수 바이너리 라는 뜻
+        // 브라우저가 열 방법을 몰라 저장 쪽으로 기울게 하는 '힌트'일 뿐, 다운로드 확정하지 못한다.
+        // (확장자 등에 따라 브라우저가 그냥 열어버릴 수도 있다.)
+        // * .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=UTF-8''"+encodedFileName)
+        // attachment는 인라인으로 열지말고 무조건 첨부(다운로드)하라는 확실한 지시
+        // filename*(별표) 는 인코딩을 명시하는 최신 문법으로 "저장될 기본 파일명"을 정한다.
+        // 이게 없으면 URL 끝의 UUID 붙음 이름으로 저장돼 버린다. 그래서 원본 이름으로 저장되게 넣는 것
+        // UTF-8 뒤에 '' : 언어필드 (ex UTF-8'ko')
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=UTF-8''"+encodedFileName)
